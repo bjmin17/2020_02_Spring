ae3a3ee3a6604242a94d368bf14527d236abd375
bjmin17 <bjmin17@naver.com>
2020-05-11 10:20:01 +0900

    2020.05.11. spring security 설정

---
+++ SpMVC28_MyShop/src/main/java/com/biz/shop/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.biz.shop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping(value = "/user")
+@Controller
+public class UserController {
+
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String login() {
+		return "auth/login";
+	}
+	
+	@RequestMapping(value="/join",method=RequestMethod.GET)
+	public String join() {
+		return "auth/join";
+	}
+}
